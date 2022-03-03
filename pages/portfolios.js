3c98c5e1e1522aa0b8cad865c34e00ca34705e9f
Lakshit Rattan <lakshit0439.cse19@chitkara.edu.in>
2022-03-04 00:52:29 +0530

    post data & Portfolio Details

--- pages/portfolios.js
+++ pages/portfolios.js
@@ -1,15 +1,42 @@
-import React from 'react';
-import BaseLayout from '../components/layouts/BaseLayout';
+import React from "react";
+import BaseLayout from "../components/layouts/BaseLayout";
+import Link from "next/link";
+
+import axios from "axios";
 
 class Portfolios extends React.Component {
+  static async getInitialProps() {
+    let posts = [];
+    try {
+      const res = await axios.get("https://jsonplaceholder.typicode.com/posts");
+      posts = res.data;
+    } catch (e) {
+      console.error(e);
+    }
+
+    return { posts: posts.slice(0, 10) };
+  }
+
+  renderPosts(posts) {
+    return posts.map((post) => (
+      <li key={post.id} style={{ fontSize: "20px" }}>
+        <Link as={`/portfolios/${post.id}`} href={`/portfolios/[id]`}>
+          <a>
+            {post.title}
+          </a>
+        </Link>
+      </li>
+    ));
+  }
+
   render() {
-    const {appProps} = this.props;
+    const { posts } = this.props;
     return (
       <BaseLayout>
-      <h1 className='customClass'>This is the Portfolios Page</h1>
-      <h2>{appProps}</h2>
+        <h1 className="customClass">This is the Portfolios Page</h1>
+        <ul>{this.renderPosts(posts)}</ul>
       </BaseLayout>
-    )
+    );
   }
 }
 
